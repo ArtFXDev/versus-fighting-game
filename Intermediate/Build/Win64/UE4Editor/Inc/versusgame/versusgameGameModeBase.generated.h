f3893331383b43f1b97c71981e97941e67e8cb42
Raphael <contact@raphaelgarcia.io>
2020-05-25 18:21:34 +0200

    buildbot commit

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/versusgame/versusgameGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VERSUSGAME_versusgameGameModeBase_generated_h
+#error "versusgameGameModeBase.generated.h already included, missing '#pragma once' in versusgameGameModeBase.h"
+#endif
+#define VERSUSGAME_versusgameGameModeBase_generated_h
+
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_15_SPARSE_DATA
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_15_RPC_WRAPPERS
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAversusgameGameModeBase(); \
+	friend struct Z_Construct_UClass_AversusgameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AversusgameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/versusgame"), NO_API) \
+	DECLARE_SERIALIZER(AversusgameGameModeBase)
+
+
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAversusgameGameModeBase(); \
+	friend struct Z_Construct_UClass_AversusgameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AversusgameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/versusgame"), NO_API) \
+	DECLARE_SERIALIZER(AversusgameGameModeBase)
+
+
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AversusgameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AversusgameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AversusgameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AversusgameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AversusgameGameModeBase(AversusgameGameModeBase&&); \
+	NO_API AversusgameGameModeBase(const AversusgameGameModeBase&); \
+public:
+
+
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AversusgameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AversusgameGameModeBase(AversusgameGameModeBase&&); \
+	NO_API AversusgameGameModeBase(const AversusgameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AversusgameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AversusgameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AversusgameGameModeBase)
+
+
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_12_PROLOG
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	versusgame_Source_versusgame_versusgameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	versusgame_Source_versusgame_versusgameGameModeBase_h_15_SPARSE_DATA \
+	versusgame_Source_versusgame_versusgameGameModeBase_h_15_RPC_WRAPPERS \
+	versusgame_Source_versusgame_versusgameGameModeBase_h_15_INCLASS \
+	versusgame_Source_versusgame_versusgameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define versusgame_Source_versusgame_versusgameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	versusgame_Source_versusgame_versusgameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	versusgame_Source_versusgame_versusgameGameModeBase_h_15_SPARSE_DATA \
+	versusgame_Source_versusgame_versusgameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	versusgame_Source_versusgame_versusgameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	versusgame_Source_versusgame_versusgameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VERSUSGAME_API UClass* StaticClass<class AversusgameGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID versusgame_Source_versusgame_versusgameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
